d9966a5dc9bcfa5edfa7897c423f88b58bb547c7
MisterJar <whitehousejared1@gmail.com>
2017-08-04 14:01:46 -0400

    added a player class, alien class, some misc stuff.

--- spacegame.cpp
+++ spacegame.cpp
@@ -19,15 +19,44 @@
 #include <iostream>
 using namespace std;
 using namespace sf;
+// Make gameplay items like money and the players and misc stuff.
+struct spaceMoney {
+	int ironMoney;
+	int goldMoney;
+	int platMoney;
+	int scrapMoney;
+};
+class player {
+	public:
+	// make player health and other stuff that the player should have.
+	int health = 100;
+	int shield = 200;
+	long int money = 0;
+
+};
+class alien {
+	public:
+	// make alien health and other stuff that the aliens should have.
+	int alienHealth = 70;
+	int alienShield = 40;
+
+};
 
 
 int main()
 {
 	RenderWindow window(VideoMode(800,600), "Spacegame");
-
+// THIS IS GOING TO BE THE PLAYER!!!! HE SHOULD BE A TRIANGLE!!! BECAUSE SPACESHIP!!!
 	CircleShape triPlayer(80,3); // The Triangle is the player
 	triPlayer.setFillColor(Color::Blue); // the colour is blue
 	triPlayer.setPosition(300,400); // the position of the Player
+
+// THIS IS GOING TO BE THE ALIEN! HE SHOULD BE A SPHERE! BECAUSE UFO!
+	CircleShape alien(60,8);
+	alien.setFillColor(Color::Red);
+	alien.setPosition(300,100);
+// THIS IS GOING TO BE THE BULLET, IT IS GOING TO BE WHITE, AND A RECTANGLE
+
 
 	while(window.isOpen())
 
@@ -60,6 +89,7 @@ int main()
 			}
 			if(Keyboard::isKeyPressed(Keyboard::Space)) // fires gun
 			{
+				cout << "pew pew goes the gun!" << endl;
 				// this will make the gun fire.
 			}
 		}
@@ -67,6 +97,7 @@ int main()
 	
 		window.clear();
 		window.draw(triPlayer);
+		window.draw(alien);
 		window.display();
 		
 	}
